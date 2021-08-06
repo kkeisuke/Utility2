4c9c32bfabbcf8d9c60b2417b772b0b81ba53c03
kimura keisuke <mail@kkeisuke.com>
2021-08-06 13:49:11 +0900

    Textarea 追加

---
+++ components/ui/Textarea.tsx
@@ -0,0 +1,11 @@
+import { ChangeEventHandler, VFC } from 'react'
+
+type TextareaProps = {
+  value: string
+  rows?: number
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>
+}
+
+export const Textarea: VFC<TextareaProps> = (props) => {
+  return <textarea className="p-2 mb-1 w-full border" value={props.value} rows={props.rows || 10} onChange={props.onChange} />
+}
