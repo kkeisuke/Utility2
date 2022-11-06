6a8a4d782579c1ccdb8bd37113ba47337e5e44d0
kimura keisuke <mail@kkeisuke.com>
2022-11-06 20:40:04 +0900

    軽微な修正

--- components/layout/AppHead.tsx
+++ components/layout/AppHead.tsx
@@ -9,7 +9,7 @@ type AppHeadProps = {
 export const AppHead: FC<AppHeadProps> = (props) => {
   return (
     <Head>
-      <title>{props.title} : Utility</title>
+      <title>{`${props.title} : Utility`}</title>
       <meta name="description" content={props.description} />
       <link rel="icon" href="/favicon.ico" />
     </Head>
